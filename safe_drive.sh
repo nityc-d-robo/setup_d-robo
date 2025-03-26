c0faebf0a1353d120ebf9a193f78b7c4d40d361d
ezomo <ezomo1daniel@gmail.com>
2025-03-26 13:13:06 +0900

    safe_drive自動化完成

--- safe_drive.sh
+++ safe_drive.sh
@@ -1,8 +1,8 @@
-sudo apt install curl gnupg2 lsb-release python3-pip git
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install -y curl gnupg2 lsb-release python3-pip git
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-sudo apt install python3-venv
+sudo apt install -y python3-venv
 python3 -m venv d-robo
 source d-robo/bin/activate
 
@@ -10,8 +10,10 @@ echo "source ~/d-robo/bin/activate" >> ~/.bashrc
 echo "source ~/d-robo/bin/activate" >> ~/.zshrc
 
 pip install git+https://github.com/tier4/colcon-cargo.git
-pip install git+https://github.com/colcon/colcon-ros-cargo.git
+pip install  git+https://github.com/colcon/colcon-ros-cargo.git
 
 git clone https://github.com/tier4/cargo-ament-build.git
 cd cargo-ament-build
 cargo install --path .
+
+cd ~/
